6d7906f05e5325fda8e33211307a0f52f227f8e8
Christopher Groskopf <chrisgroskopf@gmail.com>
2017-01-13 15:13:20 -0600

    Error handling and whitespace.

--- src/js/thing.js
+++ src/js/thing.js
@@ -13,13 +13,13 @@ var $outputSection = null;
 var $pivottable = null;
 
 var columnTemplate = _.template('\
-    <tr class="column">\
-        <td><%= columnName %></td>\
-        <td><input type="radio" name="><%= columnName %>" value="labels" /></td>\
-        <td><input type="radio" name="><%= columnName %>" value="categories" /></td>\
-        <td><input type="radio" name="><%= columnName %>" value="values" /></td>\
-        <td><input type="radio" name="><%= columnName %>" value="ignore" /></td>\
-    </tr>');
+		<tr class="column">\
+				<td><%= columnName %></td>\
+				<td><input type="radio" name="><%= columnName %>" value="labels" /></td>\
+				<td><input type="radio" name="><%= columnName %>" value="categories" /></td>\
+				<td><input type="radio" name="><%= columnName %>" value="values" /></td>\
+				<td><input type="radio" name="><%= columnName %>" value="ignore" /></td>\
+		</tr>');
 
 // Global state
 var tableData = null;
@@ -31,255 +31,268 @@ var agg = 'sum';
  * On page load.
  */
 function init() {
-  $html = $('html');
-  $body = $('body');
+	$html = $('html');
+	$body = $('body');
 
-  $uploadSection = $('#upload');
-  $csvInput = $('#upload input');
+	$uploadSection = $('#upload');
+	$csvInput = $('#upload input');
 
-  $columnSection = $('#columns');
-  $columns = $('#columns tbody');
+	$columnSection = $('#columns');
+	$columns = $('#columns tbody');
 
-  $aggSection = $('#aggregation');
-  $aggSelect = $('#aggregation select');
+	$aggSection = $('#aggregation');
+	$aggSelect = $('#aggregation select');
 
-  $outputSection = $('#output')
-  $pivottable = $('#pivottable');
+	$outputSection = $('#output')
+	$pivottable = $('#pivottable');
 
-  $html.on('dragover', onDrag);
-  $html.on('dragend', onDragEnd);
-  $html.on('dragexit', onDragEnd);
-  $html.on('dragleave', onDragEnd);
-  $html.on('drop', onDrop);
-  $csvInput.bind('change', onCSVChange);
-  $aggSelect.bind('change', onAggSelectChange);
+	$html.on('dragover', onDrag);
+	$html.on('dragend', onDragEnd);
+	$html.on('dragexit', onDragEnd);
+	$html.on('dragleave', onDragEnd);
+	$html.on('drop', onDrop);
+	$csvInput.bind('change', onCSVChange);
+	$aggSelect.bind('change', onAggSelectChange);
 
-  $uploadSection.show();
+	$uploadSection.show();
 }
 
 /*
  * When CSV file input changes.
  */
 function onCSVChange(e) {
-  parse(e.target.files[0]);
+	parse(e.target.files[0]);
 }
 
 /*
  * Dragging...
  */
 function onDrag(e) {
-  e.stopPropagation();
-  e.preventDefault();
-  e.originalEvent.dataTransfer.dropEffect = 'copy';
-  $body.addClass('drop-border');
+	e.stopPropagation();
+	e.preventDefault();
+	e.originalEvent.dataTransfer.dropEffect = 'copy';
+	$body.addClass('drop-border');
 };
 
 /*
  * Drag ended without drop.
  */
 function onDragEnd(e) {
-  e.stopPropagation();
-  e.preventDefault();
-  e.originalEvent.dataTransfer.dropEffect = 'copy';
-  $body.removeClass('drop-border');
+	e.stopPropagation();
+	e.preventDefault();
+	e.originalEvent.dataTransfer.dropEffect = 'copy';
+	$body.removeClass('drop-border');
 }
 
 /*
  * Dropped!
  */
 function onDrop(e) {
-  e.stopPropagation();
-  e.preventDefault();
-  $body.removeClass('drop-border');
-  parse(e.originalEvent.dataTransfer.files[0]);
+	e.stopPropagation();
+	e.preventDefault();
+	$body.removeClass('drop-border');
+	parse(e.originalEvent.dataTransfer.files[0]);
 }
 
 /*
  * Process file upload.
  */
 function parse(f) {
-  $columnSection.hide();
-  $columns.html('');
-  $outputSection.hide();
+	$columnSection.hide();
+	$columns.html('');
+	$outputSection.hide();
 
-  $($pivottable).html('<p align="center" style="color:grey;">(processing...)</p>')
+	$($pivottable).html('<p align="center" style="color:grey;">(processing...)</p>')
 
-  Papa.parse(f, {
-    skipEmptyLines: true,
-    complete: onParsed,
-    error: function(e) {
-      alert(e)
-    },
-  });
+	Papa.parse(f, {
+		skipEmptyLines: true,
+		complete: onParsed,
+		error: function(e) {
+			alert(e)
+		},
+	});
 }
 
 /*
  * When uploaded data has been parsed.
  */
 function onParsed(parseResult) {
-  tableData = parseResult.data;
-  columnNames = tableData[0];
+	tableData = parseResult.data;
+	columnNames = tableData[0];
 
-  _.each(columnNames, function(columnName, i) {
-    var column = columnTemplate({
-      'columnName': columnName
-    });
+	_.each(columnNames, function(columnName, i) {
+		var column = columnTemplate({
+			'columnName': columnName
+		});
 
-    var $column = $(column);
-    $columns.append($column);
+		var $column = $(column);
+		$columns.append($column);
 
-    var defaultUse = 'ignore';
+		var defaultUse = 'ignore';
 
-    if (i == 0) {
-      defaultUse = 'labels';
-    } else if (i == 1) {
-      defaultUse = 'values';
-    }
+		if (i == 0) {
+			defaultUse = 'labels';
+		} else if (i == 1) {
+			defaultUse = 'values';
+		}
 
-    $column.find('[value="' + defaultUse + '"]').attr('checked', 'checked');
-    $column.find('input').on('change', onColumnUseChange)
-  });
+		$column.find('[value="' + defaultUse + '"]').attr('checked', 'checked');
+		$column.find('input').on('change', onColumnUseChange)
+	});
 
-  // Populate global state
-  onColumnUseChange();
+	// Populate global state
+	onColumnUseChange();
 
-  $columnSection.show();
-  $aggSection.show();
-  $outputSection.show();
+	$columnSection.show();
+	$aggSection.show();
+	$outputSection.show();
 }
 
 /*
  * Column use selected.
  */
 function onColumnUseChange(e) {
-  columnUses = {};
+	columnUses = {};
 
-  _.each(columnNames, function(columnName, i) {
-    var use = $columns.find('input:checked').eq(i).val();
+	_.each(columnNames, function(columnName, i) {
+		var use = $columns.find('input:checked').eq(i).val();
 
-    columnUses[columnName] = use;
-  });
+		columnUses[columnName] = use;
+	});
 
-  pivot();
+	pivot();
 }
 
 /*
  * Select aggregator was changed.
  */
 function onAggSelectChange(e) {
-    agg = $aggSelect.val();
+	agg = $aggSelect.val();
 
-    pivot();
+	pivot();
 }
 
 /*
  * Execute pivot.
  */
 function pivot() {
-  // TODO: validate only one labels and only one categories
+	var labelColumn = null;
+	var categoryColumn = null;
+	var valueColumn = null;
 
-  var labelColumn = null;
-  var categoryColumn = null;
-  var valueColumn = null;
+	_.each(columnNames, function(columnName) {
+		var use = columnUses[columnName];
 
-  _.each(columnNames, function(columnName) {
-    var use = columnUses[columnName];
+		if (use == 'labels') {
+			if (labelColumn) {
+				alert('You may only have one label column!')
+				return;
+			}
 
-    if (use == 'labels') {
-      labelColumn = columnName;
-    } else if (use == 'categories') {
-      categoryColumn = columnName;
-    } else if (use == 'values') {
-      valueColumn = columnName;
-    }
-  });
+			labelColumn = columnName;
+		} else if (use == 'categories') {
+			if (categoryColumn) {
+				alert('You may only have one category column!')
+				return;
+			}
 
-  var pivotOptions = {
-    rows: [labelColumn],
-    aggregator: $.pivotUtilities.aggregatorTemplates[agg]()([valueColumn]),
-    renderer: atlasTSVRenderer
-  }
+			categoryColumn = columnName;
+		} else if (use == 'values') {
 
-  if (categoryColumn) {
-    pivotOptions['cols'] = [categoryColumn];
-  }
+			if (valueColumn) {
+				alert('You may only have one value column!')
+				return;
+			}
+			valueColumn = columnName;
+		}
+	});
 
-  $($pivottable).pivot(tableData, pivotOptions);
+	var pivotOptions = {
+		rows: [labelColumn],
+		aggregator: $.pivotUtilities.aggregatorTemplates[agg]()([valueColumn]),
+		renderer: atlasTSVRenderer
+	}
+
+	if (categoryColumn) {
+		pivotOptions['cols'] = [categoryColumn];
+	}
+
+	$($pivottable).pivot(tableData, pivotOptions);
 }
 
 /*
  * pivottable TSV renderer customized for Atlas.
  */
 function atlasTSVRenderer(pivotData, opts) {
-  var defaults = {
-    'localeStrings': {}
-  };
+	var defaults = {
+		'localeStrings': {}
+	};
 
-  var opts = $.extend(true, {}, defaults, opts)
+	var opts = $.extend(true, {}, defaults, opts)
 
-  var rowKeys = pivotData.getRowKeys();
+	var rowKeys = pivotData.getRowKeys();
 
-  if (rowKeys.length == 0) {
-    rowKeys.push([]);
-  }
+	if (rowKeys.length == 0) {
+		rowKeys.push([]);
+	}
 
-  var colKeys = pivotData.getColKeys();
+	var colKeys = pivotData.getColKeys();
 
-  if (colKeys.length == 0) {
-    colKeys.push([]);
-  }
+	if (colKeys.length == 0) {
+		colKeys.push([]);
+	}
 
-  var rowAttrs = pivotData.rowAttrs;
-  var colAttrs = pivotData.colAttrs;
+	var rowAttrs = pivotData.rowAttrs;
+	var colAttrs = pivotData.colAttrs;
 
-  var result = []
-  var row = []
+	var result = []
+	var row = []
 
-  _.each(rowAttrs, function(rowAttr) {
-    row.push(rowAttr);
-  });
+	_.each(rowAttrs, function(rowAttr) {
+		row.push(rowAttr);
+	});
 
-  if (colKeys.length == 1 && colKeys[0].length == 0) {
-    row.push(pivotData.aggregatorName);
-  } else {
-    _.each(colKeys, function(colKey) {
-      row.push(colKey.join("-"));
-    });
-  }
+	if (colKeys.length == 1 && colKeys[0].length == 0) {
+		row.push(pivotData.aggregatorName);
+	} else {
+		_.each(colKeys, function(colKey) {
+			row.push(colKey.join("-"));
+		});
+	}
 
-  result.push(row);
+	result.push(row);
 
-  _.each(rowKeys, function(rowKey) {
-    var row = [];
+	_.each(rowKeys, function(rowKey) {
+		var row = [];
 
-    _.each(rowKey, function(r) {
-      row.push(r);
-    });
+		_.each(rowKey, function(r) {
+			row.push(r);
+		});
 
-    _.each(colKeys, function(colKey) {
-      var agg = pivotData.getAggregator(rowKey, colKey);
-      var value = agg.value();
+		_.each(colKeys, function(colKey) {
+			var agg = pivotData.getAggregator(rowKey, colKey);
+			var value = agg.value();
 
-      if (value) {
-        row.push(value);
-      } else {
-        row.push('null');
-      }
-    })
+			if (value) {
+				row.push(value);
+			} else {
+				row.push('null');
+			}
+		})
 
-    result.push(row);
-  });
+		result.push(row);
+	});
 
-  var text = '';
+	var text = '';
 
-  _.each(result, function(r) {
-    text += r.join('\t') + '\n';
-  });
+	_.each(result, function(r) {
+		text += r.join('\t') + '\n';
+	});
 
-  return $("<textarea>").text(text) ;
+	return $("<textarea>").text(text);
 }
 
 // Bind on-load handler
 $(document).ready(function() {
-  init();
+	init();
 });
